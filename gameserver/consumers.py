8e5e426dbb535c1b8f84a77164a5444bda110359
Sakoda2269 <Ksakoda.2269@gmail.com>
2023-10-11 22:11:02 +0900

    add leave game system

--- gameserver/consumers.py
+++ gameserver/consumers.py
@@ -2,16 +2,18 @@ import json
 import random
 from collections import defaultdict
 import math
- 
+from . import models
 from channels.generic.websocket import AsyncWebsocketConsumer
- 
+import uuid
  
 class ChatConsumer(AsyncWebsocketConsumer):
     joined = defaultdict(list)
+    player_id = defaultdict(str)
     async def connect(self):
         print("connect")
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = "chat_%s" % self.room_name
+        self.user_id = uuid.uuid4()
  
         # Join room group
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
@@ -20,6 +22,13 @@ class ChatConsumer(AsyncWebsocketConsumer):
  
     async def disconnect(self, close_code):
         # Leave room group
+        leave_id = self.player_id[self.user_id]
+        print(leave_id, "left game")
+        self.joined.pop(leave_id, None)
+        self.player_id.pop(self.user_id, None)
+        await self.channel_layer.group_send( #メッセージを送信(chat_message()を使用?)
+            self.room_group_name, {"type": "chat_message", "method": "leave", "id" : leave_id, "data":{}}
+        )
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
  
     # Receive message from WebSocket
@@ -35,11 +44,12 @@ class ChatConsumer(AsyncWebsocketConsumer):
             players = list(self.joined.keys())
             send_data["pos"] = pos
             send_data["players"] = players
-            id = data["id"]
+            join_id = data["id"]
+            self.player_id[self.user_id] = join_id
             send_data["joined"] = self.joined
-            self.joined[id] = [pos["pos_x"], 1, pos["pos_z"], 0, 90, 0]
+            self.joined[join_id] = [pos["pos_x"], 1, pos["pos_z"], 0, 90, 0]
             await self.channel_layer.group_send( #メッセージを送信(chat_message()を使用?)
-                self.room_group_name, {"type": "chat_message", "method": "join", "id" : id, "data" : send_data}
+                self.room_group_name, {"type": "chat_message", "method": "join", "id" : join_id, "data" : send_data}
             )
             print(data["name"] + " joined!")
 
@@ -48,12 +58,14 @@ class ChatConsumer(AsyncWebsocketConsumer):
             pos_y = data["pos_y"]
             pos_z = data["pos_z"]
             rotate_y = math.degrees(float(data["rotate_y"]))
-            id = data["id"]
-            self.joined[id] = [pos_x, pos_y, pos_z, 0, rotate_y, 0]
-            send_data = self.joined[id]
+            updata_id = data["id"]
+            self.joined[updata_id] = [pos_x, pos_y, pos_z, 0, rotate_y, 0]
+            send_data = self.joined[updata_id]
             await self.channel_layer.group_send( #メッセージを送信(chat_message()を使用?)
-                self.room_group_name, {"type": "chat_message", "method": "updata", "id" : id, "data" : send_data}
+                self.room_group_name, {"type": "chat_message", "method": "updata", "id" : updata_id, "data" : send_data}
             )
+        
+            
  
         # Send message to room group
 
