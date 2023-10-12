a1b9ea5ab47b4cbc4ce964424127f030c97906d8
Sakoda2269 <Ksakoda.2269@gmail.com>
2023-10-13 01:24:42 +0900

    add shot system

--- gameserver/consumers.py
+++ gameserver/consumers.py
@@ -65,6 +65,22 @@ class ChatConsumer(AsyncWebsocketConsumer):
                 self.room_group_name, {"type": "chat_message", "method": "updata", "id" : updata_id, "data" : send_data}
             )
         
+        if data["method"] == "shot":
+            pos_x = data["pos_x"]
+            pos_y = data["pos_y"]
+            pos_z = data["pos_z"]
+            rotate_y = data["rotate_y"]
+            shot_id = data["id"]
+            send_data = {
+                "pos_x":pos_x,
+                "pos_y":pos_y,
+                "pos_z":pos_z,
+                "rotate_y":rotate_y
+            }
+            await self.channel_layer.group_send( #メッセージを送信(chat_message()を使用?)
+                self.room_group_name, {"type": "chat_message", "method": "shot", "id" : shot_id, "data" : send_data}
+            )
+
             
  
         # Send message to room group
